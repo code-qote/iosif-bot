f30da3595581a9ed4552a61f27217595eb3a98eb
codeC4t <49525233+codeC4t@users.noreply.github.com>
2020-09-06 20:38:53 +0400

    test

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 last_congratulation = None
+k = 0
 
 async def check_birthdays():
     await bot.wait_until_ready()
-    global last_congratulation
+    global last_congratulation, k
     while not bot.is_closed():
         day = date.today()
         for birthday in birthdays:
@@ -31,7 +32,8 @@ async def check_birthdays():
                 # await channel.send(congratulation.replace('date', birthday[3]).replace('name', birthday[2]))
                 last_congratulation = congratulation
                 break
-        print('i work')
+        k += 1
+        print(k)
         await asyncio.sleep(time_to_sleep)
 
 
