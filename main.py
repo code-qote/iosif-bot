fba74577bf18304da36a247c7ff8cd04319654c0
codeC4t <49525233+codeC4t@users.noreply.github.com>
2021-03-30 12:01:13 +0400

    meme & text translator

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
-import discord
-from discord.ext import commands
-import asyncio
-from discord import FFmpegPCMAudio
-from consts import *
-import os
-from api import *
-from data import db_session
-from commands import music, memes
-from commands.fortnite import fortnite
+from flask import Flask, request, make_response, jsonify
+from flask_restful import reqparse, abort, Api, Resource
+from flask_wtf import FlaskForm
+from flask import redirect
+from flask_wtf.csrf import CsrfProtect
+from meme_resources import *
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'butterfly'
+csfr = CsrfProtect()
+api = Api(app)
+# url_api = 'https://iosif-rest-api.herokuapp.com/api'
 
 if __name__ == '__main__':
-    db_session.global_init()
-    bot = commands.Bot(command_prefix='!')
-
-    music_cog = music.Music(bot)
-    memes_cog = memes.Memes(bot)
-    fortnite_cog = fortnite.Fortnite(bot)
-    bot.add_cog(music_cog)
-    bot.add_cog(memes_cog)
-    bot.add_cog(fortnite_cog)
-    # bot.add_cog(queue_music.QueueMusic(bot))
-    # bot.add_cog(playlist_music.PlaylistMusic(bot))
-
-    @bot.event
-    async def on_reaction_add(reaction: discord.Reaction, user):
-        server_id = reaction.message.guild.id
-        if user != bot.user:
-            if reaction.emoji == '⏹️':
-                await music_cog._stop(await bot.get_context(reaction.message))
-            elif reaction.emoji == '⏸️':
-                await music_cog._pause(await bot.get_context(reaction.message))
-            elif reaction.emoji == '⏮️':
-                await music_cog._back(await bot.get_context(reaction.message))
-            elif reaction.emoji == '⏭️':
-                await music_cog._skip(await bot.get_context(reaction.message))
-            elif reaction.emoji == '▶️':
-                await music_cog._resume(await bot.get_context(reaction.message))
-    
-    bot.run(TOKEN)
-
+    api.add_resource(TranslateImageResource, '/api/v1/translate_image')
+    app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000))
+    # app.run(port=8080, host='127.0.0.1')
---
+++ meme_resources.py
@@ -0,0 +1,79 @@
+from flask_restful import reqparse, abort, Api, Resource
+from flask import jsonify
+import cv2
+import csv
+import pytesseract
+from pytesseract.pytesseract import Output
+import asyncio
+import aiohttp
+from requests import get
+from random import randint
+from googletrans import Translator
+import os
+import numpy as np
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+parser_image = reqparse.RequestParser()
+parser_image.add_argument('image_url', required=True)
+parser_image.add_argument('language', required=True)
+
+parser_text = reqparse.RequestParser()
+parser_text.add_argument('text', required=True)
+parser_text.add_argument('language', required=True)
+
+def get_text_from_image(image_path):
+    image = cv2.imread(image_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.threshold(
+        gray_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    kernel = np.ones((2, 2), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=2)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    h, w = image.shape
+    cv2.rectangle(image, (0, h - 30), (round(w * 0.25), h), (0, 0, 0), -1)
+    parse_text = []
+    word_list = []
+    last_word = ''
+    details = pytesseract.image_to_data(image, output_type=Output.DICT, lang='eng')
+    for word in details['text']:
+        if word != '':
+            word_list.append(word)
+            last_word = word
+        if (last_word != '' and word == '') or (word == details['text'][-1]):
+            parse_text.append(word_list)
+            word_list = []
+    return parse_text
+
+class TranslateImageResource(Resource):
+    def get(self):
+        args = parser_image.parse_args()
+        image_url = args['image_url']
+        language = args['language']
+        response = get(image_url)
+        if response.status_code == 200:
+            filename = str(randint(10000, 99999)) + '.jpg'
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            text = ' '.join([' '.join(i) for i in get_text_from_image(filename)])
+            translator = Translator(service_urls=[
+                                    'translate.google.ru',
+                                    'translate.google.com',
+                                ])
+            translation = translator.translate(text, dest=language)
+            os.remove(filename)
+            return translation.text
+        else:
+            return 'There was a problem, try again.'
+
+class TranslateText(Resource):
+    def get(self):
+        args = parser_image.parse_args()
+        text = args['text']
+        language = args['language']
+        translator = Translator(service_urls=[
+            'translate.google.ru',
+            'translate.google.com',
+        ])
+        translation = translator.translate(text, dest=language)
+        return translation.text
