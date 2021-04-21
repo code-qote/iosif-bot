f3fd5cd4a0f6f8f3c5ef156919bcf306e401ca40
codeC4t <49525233+codeC4t@users.noreply.github.com>
2021-04-21 20:00:50 +0400

    big update

--- consts.py
+++ consts.py
@@ -1,9 +1,11 @@
 import os
 
-enabled_users = ['Pyatochpock', 'qote0', 'kom1ssv', 'fuw1ee', 'Llevin2005', 'Aragon_v_s']
+enabled_users = ['Pyatochpock', 'qote0']
 
 TOKEN = '1518381259:AAE_vEybSi2pT7WdPHyybYySTHWPWMT3D6M'
 PORT = int(os.environ.get('PORT', '5000'))
+
+MESSAGE_API_TOKEN = 'QHKqKGksdXMNwH9NgKkLy2NcK9cHes'
 
 #Mega
 mega_token = 'Gr0a4ZaN0Nq6Q7QfwTAr6w'
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+from requests import post
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
-from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler
+from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler, ConversationHandler
 from telegram.ext.dispatcher import run_async
 from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove, Document
 from io import BytesIO
-from mega import Mega
 from consts import *
 import logging
-import random
 import os
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -19,36 +18,47 @@ def get_callback(type, obj):
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-@run_async
-def add_photo(update, context):
+def send(message):
+    json = {'token': MESSAGE_API_TOKEN, 'message': message}
+    if post('https://iosif.herokuapp.com/send_message', json=json).status_code == 200:
+        return True
+    return False
+
+def message_start(update, context):
+    if update.message.from_user['username'] in enabled_users:   
+        update.message.reply_text('Отправьте мне текст сообщения')
+    return 1
+
+def message_receive(update, context):
+    if send(update.message.text):
+        update.message.reply_text('Отправлено')
+    else:
+        update.message.reply_text('Произошла ошибка')
+    return ConversationHandler.END
+
+def start(update, context):
     if update.message.from_user['username'] in enabled_users:
-        photos = update.message.photo
-        photo_file = photos[-1].get_file()
-        filename = f'meme_' + photo_file.file_path.split('/')[-1]
-        photo_file.download(filename)
-        mega = Mega()
-        m = mega.login(mega_email, mega_password)
-        folder = m.find('memes')
-        m.upload(filename, folder[0])
-        os.remove(filename)
-        update.message.reply_text('Мем загружен.')
+        update.message.reply_text('Я отправлю ваше сообщение всем')
     else:
         update.message.reply_text('Ты не можешь пользоваться этим ботом!')
 
-@run_async
-def start(update, context):
-    if update.message.from_user['username'] in enabled_users:
-        update.message.reply_text('Пришли мне мем!')
-    else:
-        update.message.reply_text('Ты не можешь пользоваться этим ботом!')
+def cancel(update, context):
+    update.message.reply_text('Ок, передумали получается')
+    return ConversationHandler.END
 
 def main():
     updater = Updater(TOKEN, use_context=True)
     updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
     updater.bot.set_webhook('https://iosif-telegram.herokuapp.com/' + TOKEN)
     dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.photo, add_photo))
+    conv = ConversationHandler(
+        entry_points=[CommandHandler('message', message_start)],
+        states = {1: [MessageHandler(Filters.text & ~Filters.command, message_receive)]},
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
     dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(conv)
     updater.start_polling()
 
     updater.idle()
