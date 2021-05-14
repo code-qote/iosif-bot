9be0015768d5ab9890821e8039181e09b8b925d8
codeC4t <49525233+codeC4t@users.noreply.github.com>
2021-05-14 18:41:26 +0400

    playlists update

--- data/__all_models.py
+++ data/__all_models.py
@@ -4,7 +4,13 @@ from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects import sqlite, postgresql
 
+def create_tsvector(*args):
+    exp = args[0]
+    for e in args[1:]:
+        exp += ' ' + e
+    return sqlalchemy.func.to_tsvector('english', exp)
 
 class Track_db(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'tracks'
@@ -18,6 +24,15 @@ class Track_db(SqlAlchemyBase, SerializerMixin):
     artist = sqlalchemy.Column(sqlalchemy.String(length=100), nullable=False)
     server_id = sqlalchemy.Column(sqlalchemy.String, nullable=False)
 
+
+class Default_track_db(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'default_tracks'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String(length=100), nullable=False)
+    artist = sqlalchemy.Column(sqlalchemy.String(length=100), nullable=False)
+
 class Holiday(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'holiday'
 
@@ -25,3 +40,23 @@ class Holiday(SqlAlchemyBase, SerializerMixin):
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String(length=100), nullable=False)
 
+class Playlist_db(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'playlists'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    uri = sqlalchemy.Column(sqlalchemy.String(length=100), nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    to_search = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    __ts_vector__ = create_tsvector(
+        sqlalchemy.cast(sqlalchemy.func.coalesce(to_search, ''), postgresql.TEXT)
+    )
+
+    __table_args__ = tuple([(
+        sqlalchemy.Index(
+            'idx_person_fts',
+            __ts_vector__,
+            postgresql_using='gin'
+        )
+    )])
---
+++ errors_handler.py
@@ -0,0 +1,44 @@
+from discord.ext import commands
+import discord
+
+
+async def send_info(ctx, title, message):
+    await ctx.send(embed=discord.Embed(title=title, description=message, color=discord.Color.greyple()))
+
+
+async def send_error(ctx, message):
+    await ctx.send(embed=discord.Embed(title=message, color=discord.Color.red()))
+
+
+async def send_success(ctx, message):
+    await ctx.send(embed=discord.Embed(title=message, color=discord.Color.green()))
+
+class CommandErrorHandler(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        cog = ctx.cog
+        if cog:
+            if cog._get_overridden_method(cog.cog_command_error) is not None:
+                return
+
+        ignored = (commands.CommandNotFound, )
+
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, ignored):
+            return
+        
+        if isinstance(error, commands.ArgumentParsingError):
+            await send_error(ctx, 'You did not pass any arguments.')
+        elif isinstance(error, commands.ChannelNotFound):
+            await send_error(ctx, 'Join any voice channel.')
+        elif isinstance(error, commands.MessageNotFound):
+            pass 
